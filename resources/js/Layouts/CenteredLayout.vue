4a1cfecef9de8f9140ed512ebfb5791fe15da174
Rafał Prymus <prymus.it@gmail.com>
2024-06-16 15:50:20 +0200

    fix for centered layout

--- resources/js/Layouts/CenteredLayout.vue
+++ resources/js/Layouts/CenteredLayout.vue
@@ -2,6 +2,8 @@
     <div
         class="relative flex min-h-screen w-screen items-center justify-center bg-gradient-to-b from-orange-100 to-orange-200 p-2 pb-12"
     >
-        <slot />
+        <div class="flex w-full max-w-screen-sm justify-center">
+            <slot />
+        </div>
     </div>
 </template>
